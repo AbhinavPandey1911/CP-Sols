723e08073fa21de2099ebd084a5a05fd9850747f
AbhinavPandey1911 <75622304+AbhinavPandey1911@users.noreply.github.com>
2022-06-26 21:11:40 +0530

    Committing files

---
+++ CodeChef/C++14/KEPLERSLAW/55991509.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+ float t1,t2,n1,n2;
+ cin>>t1>>t2>>n1>>n2;
+ if((t1*t1)/(n1*n1*n1)==(t2*t2)/(n2*n2*n2)) cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;
+}
+
+    return 0;
+
+}
