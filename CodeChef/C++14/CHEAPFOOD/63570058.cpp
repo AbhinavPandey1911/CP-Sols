6d343a6555ee6bc36108bad8938b489b6d44d414
AbhinavPandey1911 <75622304+AbhinavPandey1911@users.noreply.github.com>
2022-06-26 21:11:18 +0530

    Committing files

---
+++ CodeChef/C++14/CHEAPFOOD/63570058.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    double x;
+    cin>>x;
+    int k=x/10;
+    cout<<max(k,100)<<endl;
+
+
+  }
+
+return 0;
+}
