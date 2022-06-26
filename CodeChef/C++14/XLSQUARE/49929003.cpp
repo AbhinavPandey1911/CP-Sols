f109446cce929b14c1bac06bd152b9ead86a74b0
AbhinavPandey1911 <75622304+AbhinavPandey1911@users.noreply.github.com>
2022-06-26 21:11:44 +0530

    Committing files

---
+++ CodeChef/C++14/XLSQUARE/49929003.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{ int t;
+cin>>t;
+while(t--)
+{int n,a;
+cin>>n>>a;
+  cout<<(int)sqrt(n)*a<<endl;
+
+}
+
+
+    return 0;
+}
