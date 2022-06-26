dc014cda853639f2df0415397e72428fc2243894
AbhinavPandey1911 <75622304+AbhinavPandey1911@users.noreply.github.com>
2022-06-26 21:11:20 +0530

    Committing files

---
+++ CodeChef/C++14/POLTHIEF/63264393.cpp
@@ -0,0 +1,16 @@
+#include<map>
+#include <iostream>
+using namespace std;
+int main() {
+  int t;
+  cin>>t;
+       while(t--){
+          int x,y;
+          cin>>x>>y;
+          cout<<abs(y-x)<<endl;
+   
+
+       }
+      
+     return 0;
+    }
