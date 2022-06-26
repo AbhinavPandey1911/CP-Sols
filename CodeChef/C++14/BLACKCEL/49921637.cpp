5bec233ad6bcc93875bbe0e5e0c772c5f94a95a1
AbhinavPandey1911 <75622304+AbhinavPandey1911@users.noreply.github.com>
2022-06-26 21:11:45 +0530

    Committing files

---
+++ CodeChef/C++14/BLACKCEL/49921637.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ int n,sum=0;
+cin>>n;
+cout<<(n*n)/2.0<<endl;
+
+    return 0;
+}
